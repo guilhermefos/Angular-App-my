93adc0468a2cb2ed394a9dbb9d3abb208488f9df
Guilherme Silva <guilhermefos.developer@gmail.com>
2019-05-08 10:09:20 -0300

    Setup Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAcKGLplFfFIZClanyJEJZVNn2NkM--ZpU",
+    authDomain: "study-fe281.firebaseapp.com",
+    databaseURL: "https://study-fe281.firebaseio.com",
+    projectId: "study-fe281",
+    storageBucket: "study-fe281.appspot.com",
+    messagingSenderId: "934900281528",
+    appId: "1:934900281528:web:573a83b870d6b5a8"
+  }
 };
 
 /*
